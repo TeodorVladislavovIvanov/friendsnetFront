5a3b67cbf09f65858c80b2c66f5c16a9881cd162
unknown <Teodor.Vladislavov@iesdoctorbalmis.com>
2018-07-31 18:54:56 +0200

    Added components

--- friendsNetFront/src/app/app.module.ts
+++ friendsNetFront/src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { FriendsComponent } from './users/friends/friends.component';
+import { PostComponent } from './posts/post/post.component';
+import { UserComponent } from './users/user/user.component';
+import { EventComponent } from './events/event/event.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    UsersComponent,
+    PostsComponent,
+    FriendsComponent,
+    PostComponent,
+    UserComponent,
+    EventComponent
   ],
   imports: [
     BrowserModule
