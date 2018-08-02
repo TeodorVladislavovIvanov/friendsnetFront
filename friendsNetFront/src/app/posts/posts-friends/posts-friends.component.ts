d460bf4a45c5c18fe06bd1962d55332837810c7b
unknown <Teodor.Vladislavov@iesdoctorbalmis.com>
2018-08-02 20:34:23 +0200

    adding post list to postfriends

--- friendsNetFront/src/app/posts/posts-friends/posts-friends.component.ts
+++ friendsNetFront/src/app/posts/posts-friends/posts-friends.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../shared/post.model';
+import { PostService } from '../shared/post.service';
 
 @Component({
   selector: 'app-posts-friends',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsFriendsComponent implements OnInit {
 
-  constructor() { }
+  myPosts: Post[];
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
+    this.postService.getPostList()
+      .subscribe((data: Post[]) => this.myPosts = data,
+        error => console.error(error),
+        () => console.log('Posts Loaded!')
+      );
   }
 
 }
